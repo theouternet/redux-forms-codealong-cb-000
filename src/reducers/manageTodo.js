f81806884962ce1ecf53f40eac7b3e796cc3b728
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 23:22:23 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,7 +2,12 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
 
-  console.log("reducer received this action:", action);
+
+switch (action.type) {
+  case 'ADD_TODO':
+    console.log
+}
+
 
   return state;
 }
